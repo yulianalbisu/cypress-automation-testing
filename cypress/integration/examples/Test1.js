66c3191edd9778fbb6a2be70fd47bafd29cf7a5d
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2023-01-05 10:39:39 -0500

    cypress first test

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,5 @@
+//cypress - Spec
+describe('my firs test suite', function(){it('My first test case', function() {
+    //test step
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+})})
