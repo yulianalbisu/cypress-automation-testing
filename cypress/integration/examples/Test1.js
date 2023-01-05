ec0d2c436c76a267afa4242b24bdcadc5b3ec4ea
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2023-01-05 17:02:06 -0500

    printing with log and assign alias to an html element

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,14 +10,22 @@ describe('my firs test suite', function()
     cy.wait(2000)
     cy.get('.product:visible').should('have.length',4)
     //parent child chaining
-    cy.get('.products').find('.product').should('have.length',4)
+    cy.get('.products').as('parentProduct')
+    cy.get('@parentProduct').find('.product').should('have.length',4)
     cy.get(':nth-child(3) > .product-action > button').click()
-    cy.get('.products').find('.product').eq(2).contains('ADD TO CART')
+    cy.get('@parentProduct').find('.product').eq(2).contains('ADD TO CART')
     //iterating each element
-    cy.get('.products').find('.product').each(($el, index, $list) => {
+    cy.get('@parentProduct').find('.product').each(($el, index, $list) => {
     const textVeg = $el.find('h4.product-name').text()
     if(textVeg.includes('Cashews')){
         cy.wrap($el).find('button').click()
     }
+    })
+    //assert if logo text is correctly display
+    cy.get('.brand').should('have.text', 'GREENKART')
+    const logo = cy.get('.brand').then(function(logoelement)
+    {
+        // Print in logs, text is not a cypress command is a variable method
+cy.log(logoelement.text())
     })
 })})
