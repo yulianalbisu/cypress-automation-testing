c89847bcd8b44cdd806afadd6a8f656775d1e3a3
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2023-01-05 12:22:47 -0500

    cypress wait and cypress wait for css

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,12 @@
+/// <reference types="Cypress" />
 //cypress - Spec
-describe('my firs test suite', function(){it('My first test case', function() {
+
+describe('my firs test suite', function()
+{
+    it('My first test case', function() {
     //test step
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.products').should('have.length',4)
 })})
