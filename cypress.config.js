e81ee0f7513d76a316db6a9e480209353456d158
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2023-01-05 15:15:02 -0500

    using click, wrap element when using find, not allowed click on it

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'gu93db',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -5,8 +5,19 @@ describe('my firs test suite', function()
 {
     it('My first test case', function() {
     //test step
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
-    cy.get('.products').should('have.length',4)
+    cy.get('.product:visible').should('have.length',4)
+    //parent child chaining
+    cy.get('.products').find('.product').should('have.length',4)
+    cy.get(':nth-child(3) > .product-action > button').click()
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART')
+    //iterating each element
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+    const textVeg = $el.find('h4.product-name').text()
+    if(textVeg.includes('Cashews')){
+        cy.wrap($el).find('button').click()
+    }
+    })
 })})
