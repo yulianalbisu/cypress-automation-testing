b2256f3254441050c4a9baf58fe7399e73d2bc89
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2023-01-06 12:28:03 -0500

    remove attribute, so we can open another page in the same and not other tab

---
+++ cypress/integration/examples/Test4.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+describe('My third test with checkboxes', function() {
+    it('Checkboxes', function() {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        
+        cy.get('#alertbtn').click()
+        cy.get('[value="Confirm"]').click()
+
+        //window:alert
+        cy.on('window:alert', (str)=> {
+            expect(str).to.equal('Hello , share this practice page and share your knowledge')
+        })
+        //window confirmation
+        cy.on('window:confirm', (str)=> {
+            expect(str).to.equal('Hello , Are you sure you want to confirm?')
+        })
+
+        cy.get('#opentab').invoke('removeAttr', 'target').click()
+
+
+
+
+
+    })
+})
