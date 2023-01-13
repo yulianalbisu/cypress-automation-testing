415d1de7c3f6052239ecd4c92663a26c1440ff56
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2023-01-13 10:39:59 -0500

    Using azure sql to automate inputs and store variables

--- cypress/integration/examples/SessionTest.js
+++ cypress/integration/examples/SessionTest.js
@@ -1,28 +1,99 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
+
+//const neatCSV = require('neat-csv')
+
+import neatCSV from 'neat-csv';
+
+let productName
+
+describe('JWT Session', () => {
+
+  it('is logged in through local storage', async() => {
 
 
-describe('trying to purchase', () => 
-{
-    before(()=>{
-        cy.loginAPI();
-    });
-    it('My click Add to cart button', () => {
-        const token = Cypress.env("token");
-        const url = Cypress.env("url") + "/client/";
-        cy.visit(url, 
-            { 
-                onBeforeLoad : (window)=> {
-                    window.localStorage.setItem("token", token);
-                } 
+
+    cy.LoginAPI().then(function()
+
+    {
+
+        cy.visit("https://rahulshettyacademy.com/client",
+
+        {
+
+            onBeforeLoad :function(window)
+
+            {
+
+                window.localStorage.setItem('token',Cypress.env('token'))
+
             }
-        );
-        cy.get(".card-body button:last-of-type").eq(0).click();
-        
-    });
-    
-    it('should load cart page', () => {
-        cy.get('[routerlink="/dashboard/cart"]').click();
-    });
 
-}
-)
+
+
+        })       
+
+
+
+    })
+
+    cy.get(".card-body b").eq(1).then(function(ele)
+
+      {
+
+      productName =  ele.text();
+
+      })
+
+    cy.get(".card-body button:last-of-type").eq(1).click();
+
+    cy.get("[routerlink*='cart']").click();
+
+    cy.contains("Checkout").click();
+
+    cy.get("[placeholder*='Country']").type("ind")
+
+    cy.get('.ta-results button').each(($e1, index, $list) => {
+
+
+
+      if($e1.text()===" India")
+
+      {
+
+          cy.wrap($e1).click()
+
+      }
+
+  })
+
+    cy.get(".action__submit").click();
+
+    cy.wait(2000)
+
+    cy.get(".order-summary button").click();
+
+   
+
+  cy.readFile(Cypress.config("fileServerFolder")+"/cypress/downloads/order-invoice_rahul.csv")
+
+  .then(async(text)=>
+
+  {
+
+    const csv =  await neatCSV(text)
+
+    console.log(csv)
+
+    const actualProductCSV = csv[0]["Product Name"]
+
+    expect(productName).to.equal(actualProductCSV)
+
+
+
+ 
+
+  })
+
+  })
+
+  })
